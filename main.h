990e8941bb9b965a7974154bfd759eb29a46eaf8
VengefulHummingbird0403 <hoveronoh@gmail.com>
2023-07-25 19:27:03 -0700

    main.h

--- main.h
+++ main.h
@@ -1,12 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* header files*/
 #include <stdarg.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 
-
+/* function prototypes*/
 int _printf(const char *format, ...);
 void print_arg(va_list args, const char *format, int i, int *char_count);
 int _putchar(char c);
